4f15f9d75aa68a742502ea48ebbf9f4d7bc0aad0
johlbaum <julien.ohlbaum@gmail.com>
2024-12-22 19:20:01 +0100

    Refactoring: Created a method to generate a video game with reviews.

--- tests/Unit/CalculateAverageRatingTest.php
+++ tests/Unit/CalculateAverageRatingTest.php
@@ -12,15 +12,8 @@ final class CalculateAverageRatingTest extends TestCase
      */
     public function testCalculateAverage(array $ratings, ?int $expectedAverage)
     {
-        // On crée une instance de VideoGame.
-        $videoGame = new VideoGame();
-
-        // On crée et ajoute des objets Review au jeu vidéo avec les notes associées. 
-        foreach ($ratings as $rating) {
-            $review = new Review();
-            $review->setRating($rating);
-            $videoGame->getReviews()->add($review);
-        }
+        // On crée un jeu et on lui ajoute des notes.
+        $videoGame = $this->createVideoGameWithRatings($ratings);
 
         // On crée une instance de RatingHandler qui est responsable de la gestion des notes.
         $ratingHandler = new RatingHandler();
@@ -32,16 +25,36 @@ final class CalculateAverageRatingTest extends TestCase
         $this->assertSame($expectedAverage, $videoGame->getAverageRating());
     }
 
+    private function createVideoGameWithRatings(array $ratings): VideoGame
+    {
+        $videoGame = new VideoGame();
+
+        foreach ($ratings as $rating) {
+            $review = (new Review())->setRating($rating);
+            $videoGame->getReviews()->add($review);
+        }
+
+        return $videoGame;
+    }
+
     public function provideRatingsAndExpectedAverage(): array
     {
         return [
-            'no reviews' => [[], null], // Aucun avis : moyenne null.
-            'one review' => [[5], 5],  // Une seule note : moyenne égale à la note.
-            'multiple reviews' => [[3, 5, 2, 5, 5, 4, 3, 2, 4, 1, 1], 4], // Plusieurs notes : moyenne correcte.
-            'rounding up' => [[3, 4], 4], // Arrondi vers le haut : (3 + 4) / 2 = 4.
+            // Aucun avis : moyenne null.
+            'no reviews' => [[], null],
+
+            // Une seule note : moyenne égale à la note.
+            'one review' => [[5], 5],
+
+            // Plusieurs notes : moyenne correcte.
+            'multiple reviews' => [[3, 5, 2, 5, 5, 4, 3, 2, 4, 1, 1], 4],
+
+            // Arrondi vers le haut : (3 + 4) / 2 = 4.
+            'rounding up' => [[3, 4], 4],
+
             // On teste les bornes (les valeurs minimales et maximales).
-            'minimum rating' => [[1], 1], // Note minimale.
-            'maximum rating' => [[5, 5, 5], 5], // Note maximale.
+            'minimum rating' => [[1], 1],
+            'maximum rating' => [[5, 5, 5], 5],
         ];
     }
 }
